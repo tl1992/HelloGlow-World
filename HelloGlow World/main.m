afc65a9db53e958293503854fcbde10a17834381
Tom <tom@Toms-Mac.local>
2014-02-13 07:11:32 -0800

    Initial Commit

---
+++ HelloGlow World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloGlow World
+//
+//  Created by Tom on 2/13/14.
+//  Copyright (c) 2014 tom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
